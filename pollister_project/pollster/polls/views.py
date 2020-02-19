184546cd2c4a8cb9bbc200a94b6bc0f463caca82
toriwat328 <toriwat328@gmail.com>
2020-02-18 19:31:58 -0500

    got question and both detail and result buttons to show on localhost - now creating templates for detail and view pages

--- pollister_project/pollster/polls/views.py
+++ pollister_project/pollster/polls/views.py
@@ -12,9 +12,14 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 # Show specific question and choices
-def detail(request):
+def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
     return render(request, 'polls/results.html', {'question': question})
+
+# Get question and display results
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', { 'question': question })
