03a644e1441ed6916ace3fea78e7363cdc9be13b
toriwat328 <toriwat328@gmail.com>
2020-02-18 19:15:04 -0500

    created views and routes for index, details and results. Got question to display in index.html

--- pollister_project/pollster/polls/views.py
+++ pollister_project/pollster/polls/views.py
@@ -7,4 +7,14 @@ from .models import Question, Choice
 #Get questions and display them
 
 def index(request):
-    return render(request, 'polls/index.html')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+# Show specific question and choices
+def detail(request):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/results.html', {'question': question})
